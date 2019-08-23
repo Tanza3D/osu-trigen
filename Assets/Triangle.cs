8b22691e86d85f04ac0568c89b75790ada3bf163
Hubza <33783503+Hubza@users.noreply.github.com>
2019-08-23 19:25:55 +0100

    added UI to change triangles and added triweek scene for triweek

--- Assets/Triangle.cs
+++ Assets/Triangle.cs
@@ -16,8 +16,22 @@ public class Triangle : MonoBehaviour
     public float FinalOpacity;
     public float StartX;
     public float StartY;
-    void Start()
+    // Sliders
+    public UnityEngine.UI.Slider S_YSpeedMin;
+    public UnityEngine.UI.Slider S_YSpeedMax;
+    public UnityEngine.UI.Slider S_OpacityMin;
+    public UnityEngine.UI.Slider S_OpacityMax;
+    public UnityEngine.UI.Slider S_ScaleMin;
+    public UnityEngine.UI.Slider S_ScaleMax;
+    
+    public void Init()
     {
+        YSpeedMin  =  S_YSpeedMin.value;
+        YSpeedMax  =  S_YSpeedMax.value;
+        OpacityMin =  S_OpacityMin.value;
+        OpacityMax =  S_OpacityMax.value;
+        ScaleMin   =  S_ScaleMin.value;
+        ScaleMax   =  S_ScaleMax.value;
         SpriteRenderer spRend = transform.GetComponent<SpriteRenderer>();
         Color col = spRend.color;
         FinalOpacity = Random.Range(OpacityMin, OpacityMax);
@@ -29,13 +43,48 @@ public class Triangle : MonoBehaviour
         FinalSpeed = Random.Range(YSpeedMin, YSpeedMax); //Sets FinalSpeed to a random number between YSpeedMin and YSpeedMax
         transform.position = new Vector3(StartX,StartY,0); //Sets the position to StartX and StartY, sets Z to 0
         transform.localScale = new Vector3(FinalScale, FinalScale, 1); //Sets the scale to FinalScale
+        S_YSpeedMin.onValueChanged.AddListener(SET_S_YSMIN);
+        S_YSpeedMax.onValueChanged.AddListener(SET_S_YSMAX);
+        S_OpacityMin.onValueChanged.AddListener(SET_S_OMIN);
+        S_OpacityMax.onValueChanged.AddListener(SET_S_OMAX);
+        S_ScaleMin.onValueChanged.AddListener(SET_S_SMIN);
+        S_ScaleMax.onValueChanged.AddListener(SET_S_SMAX);
     }
     void Update()
     {
         transform.position += Vector3.up * Time.deltaTime * FinalSpeed; //Moves up at speed of FinalSpeed
         if (transform.position.y >= 20) { 
-            print("uwu");
             Destroy(gameObject);
         }
+    }
+    void SET_S_YSMIN(float value)
+    {
+        
+        YSpeedMin = value;
+    }
+    void SET_S_YSMAX(float value)
+    {
+        
+        YSpeedMax = value;
+    }
+    void SET_S_OMIN(float value)
+    {
+        
+        OpacityMin = value;
+    }
+    void SET_S_OMAX(float value)
+    {
+        
+        OpacityMax = value;
+    }
+    void SET_S_SMIN(float value)
+    {
+        
+        ScaleMin = value;
+    }
+    void SET_S_SMAX(float value)
+    {
+        
+        ScaleMax = value;
     }
 }
--- Assets/TriangleSpawner.cs
+++ Assets/TriangleSpawner.cs
@@ -8,10 +8,18 @@ public class TriangleSpawner : MonoBehaviour
     public float SpawnSpeed;
     public GameObject triangle;
     public float SpawnSpeedFinal;
+    [SerializeField] UnityEngine.UI.Slider spawnSpeedSlider;
+    [SerializeField] UnityEngine.UI.Slider S_YSpeedMin;
+    [SerializeField] UnityEngine.UI.Slider S_YSpeedMax;
+    [SerializeField] UnityEngine.UI.Slider S_OpacityMin;
+    [SerializeField] UnityEngine.UI.Slider S_OpacityMax;
+    [SerializeField] UnityEngine.UI.Slider S_ScaleMin;
+    [SerializeField] UnityEngine.UI.Slider S_ScaleMax;
     // Start is called before the first frame update
     void Start()
     {
-        
+        spawnSpeedSlider.onValueChanged.AddListener(SetSlider);
+        SpawnSpeed = spawnSpeedSlider.value;
     }
 
     // Update is called once per frame
@@ -20,8 +28,19 @@ public class TriangleSpawner : MonoBehaviour
         SpawnSpeedFinal = Random.Range(0, SpawnSpeed);
         if (SpawnSpeedFinal > 5)
         {
-            print("owo");
-            Instantiate(triangle, new Vector3(0, 0, 0), Quaternion.identity);
+            
+            GameObject go = Instantiate(triangle, new Vector3(0, 0, 0), Quaternion.identity);
+            Triangle tri = go.GetComponent<Triangle>();
+            tri.S_YSpeedMin =  S_YSpeedMin;
+            tri.S_YSpeedMax =  S_YSpeedMax;
+            tri.S_OpacityMin = S_OpacityMin;
+            tri.S_OpacityMax = S_OpacityMax;
+            tri.S_ScaleMin =   S_ScaleMin;
+            tri.S_ScaleMax =   S_ScaleMax;
+            tri.Init();
         }
+    }
+    void SetSlider(float value){
+        SpawnSpeed = value;
     }
 }
