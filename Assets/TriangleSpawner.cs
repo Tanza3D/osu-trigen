917f44680565044f5bd38cae6954579d9fbe41d6
Hubza <33783503+Hubza@users.noreply.github.com>
2019-08-23 13:33:55 +0100

    Added code

---
+++ Assets/TriangleSpawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriangleSpawner : MonoBehaviour
+{
+
+    public float SpawnSpeed;
+    public GameObject triangle;
+    public float SpawnSpeedFinal;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SpawnSpeedFinal = Random.Range(0, SpawnSpeed);
+        if (SpawnSpeedFinal > 5)
+        {
+            print("owo");
+            Instantiate(triangle, new Vector3(0, 0, 0), Quaternion.identity);
+        }
+    }
+}
