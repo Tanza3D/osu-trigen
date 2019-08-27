dc88284018e1ee7545328ced4250a9d9b954183f
Hubza <33783503+Hubza@users.noreply.github.com>
2019-08-27 22:55:22 +0100

    Added latest release to repo

---
+++ Assets/UIHideScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIHideScript : MonoBehaviour
+{
+
+    public GameObject menu; // Assign in inspector
+    private bool isShowing;
+
+    void Update()
+    {
+        if (Input.GetKeyDown("escape"))
+        {
+            isShowing = !isShowing;
+            menu.SetActive(isShowing);
+        }
+    }
+}
