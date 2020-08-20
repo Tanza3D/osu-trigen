7cc0066e18db6c38853a8f410f6073ab0a91d821
Hubz <33783503+Hubza@users.noreply.github.com>
2020-08-20 10:01:21 +0100

    fixing file structure : part 1

--- Assets/Scripts/osumode/osumodecontroller.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public static class osumodecontroller{
-    public static float omode = 0;
-    public static string hex = "FFFFFF";
-}
